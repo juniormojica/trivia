fa2d607dab6fe13e41f2cf184734bec2a81f0f23
Junior Mojica <juniormojica26@gmail.com>
2021-08-30 16:12:17 -0500

    se muestran las preguntas dinamicamente en el dom

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 
-
+//trayendo los elementos html
 const questionsForm = document.getElementById("questionsForm")
 
 const startButton = document.getElementById("startButton")
+const questionsAnswers = document.getElementById("questionsAnswers")
+const firstQuestion = document.getElementById("firstQuestion")
+const secondQuestion = document.getElementById("secondQuestion")
+const thirdQuestion = document.getElementById("thirdQuestion")
+const fourthQuestion = document.getElementById("fourthQuestion")
 
 
+
+
+//funciones
+const  fillQuestions = questionsAPI=>{ questions = questionsAPI ; }
+
+
+
+//funcion principal de la trivia
+let questions = [];
 const BringQuestions = (e)=>{
-
+  
     e.preventDefault();
     const API = "https://opentdb.com/api.php?" ; 
 
@@ -18,22 +32,84 @@ const BringQuestions = (e)=>{
 
     const userAPI = `${API}amount=${numberOfQuestions.value}&category=${selecCategory.value}&difficulty=${dificulty.value}&type=${typeOfQuestion.value}`
 
-    let questions = [];
+    
     fetch(userAPI)
     .then(response => response.json())
-    .then(result => fillQuestions((result.results)))
+    .then(resultado =>  fillQuestions(resultado.results))
+   
     .catch(error => console.log(error))
+   
 
-   console.log(userAPI)
+    console.log(userAPI)
+  // show the questions on screen 
+    const fillQuestions = (questionsApi)=>{
+        questions = questionsApi;
+
+        console.log(questions)
+        questionsAnswers.innerHTML= "";
+        for (const element in questions) {
+            
+            const answerContainer= document.createElement("div")
+            answerContainer.classList.add("answer-container")
+
+            const visibleAnswer = document.createElement("h4")
+            visibleAnswer.classList.add("visible-answer")
+            visibleAnswer.innerText = `${questions[element].question}`
+
+            const visibleQuestions = document.createElement("h3")
+            visibleQuestions.classList.add("visible-question")
+            visibleQuestions.innerText = `pregunta ${element}, es ${questions[element].difficulty} de la categoria ${questions[element].category}  `
+           
+            
+
+            const answerOptions = document.createElement("div")
+            answerOptions.classList.add("answer-options")
+            const eachAnswerOption = document.createElement("h4")
+            eachAnswerOption.classList.add("each-answer-option")
+            
+
+
+
+            questionsAnswers.appendChild(answerContainer)
+            questionsAnswers.appendChild(answerOptions)
+            answerContainer.appendChild(visibleQuestions)
+            visibleQuestions.appendChild(visibleAnswer)
+            visibleAnswer.appendChild(eachAnswerOption)
+            
+            for (const key in questions) {
+
+                if(questions[key].type === "boolean") { 
+                    eachAnswerOption.innerHTML = ` <div class=""answer-options"">
+                    <input  type="button" id="firstQuestion" value=" ${questions[key].incorrect_answers}">
+                    <input  type="button" id="secondQuestion" value=" ${questions[key].incorrect_answers}">`
+                }
+
+                else{ 
+                    eachAnswerOption.innerHTML = ` <div class=""answer-options"">
+                    <input  type="button" id="firstQuestion" class="button-style" value=" ${questions[key].incorrect_answers}">
+                    <input  type="button" id="secondQuestion" class="button-style" value=" ${questions[key].incorrect_answers}">
+                    <input  type="button" id="thirdQuestion" class="button-style" value=" ${questions[key].incorrect_answers}">
+                    <input  type="button" id="fourthQuestion" class="button-style" value=" ${questions[key].incorrect_answers}">`
+                }
+                
+               
+             
+            }
+             
+        }
+        
+        
+    }
+
+
+  
     
 }
 
-const fillQuestions = (questionsAPI)=>{
-    questions = questionsAPI ; 
-    console.log(questions)
-}
 
-fillQuestions()
+
+
+
 
 
 
